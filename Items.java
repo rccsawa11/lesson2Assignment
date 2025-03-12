54eb73811ae7c85a70a820c5c21278197085d701
rccsawa11 <lsawas1@student.rccd.edu>
2025-03-12 15:00:07 -0700

    Update Items.java

--- Items.java
+++ Items.java
@@ -1,12 +1,18 @@
+import java.util.Scanner; 
+
 class Items 
 
 {  
     String title; 
     int publicationYear; 
     public static void main(String[] args) 
-
     {
-        System.out.println("Im testing out");
+        System.out.println("type in testing scanner");
+        Scanner stringScanner = new Scanner(System.in);
+        String title = stringScanner.next();
+        System.out.println("The title of the book you want is: " + title);
+
+        
     }
 
 }
